1b401ed28a78d9b338866f8313e4744e8e85da09
matteodragotto <matteodrag8@gmail.com>
2025-02-06 13:10:13 +0100

    [feat] router

---
+++ routes/movies.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const movieController = require('../controllers/movieController')
+const router = express.Router();
+
+//index
+router.get('/', movieController.index);
+
+//show
+router.get('/:id', movieController.show);
+
+//store
+router.post('/', movieController.store);
+
+//update
+router.put('/:id', movieController.update);
+
+//modify
+router.patch('/:id', movieController.modify);
+
+//destroy
+router.delete('/:id', movieController.destroy)
+
+module.exports = router
